c5b10cc7a3d7325fe7fcf38a22978363c0a10a87
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-11-29 14:21:43 +0700

    feat: add link dashboard at root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import LoadingProvider from "@/components/loadingProvider";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <LoadingProvider>
       <section>
         <h1>init</h1>
+        <Link href="/dashboard">Dashboard admin</Link>
       </section>
     </LoadingProvider>
   );
